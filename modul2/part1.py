c19e3623f73c5fb6e6d6113b93f183fbbf2fe993
emanuel.craciun <emanuel.craciun@savnet.ro>
2022-10-13 12:29:10 +0300

    homework 2

--- modul2/part1.py
+++ modul2/part1.py
@@ -25,7 +25,6 @@ print(result)
 # print multiple args
 print('Emanuel', 'Ion', 'Illia')
 
-
 # casting
 # result = 'abcd'
 # result = int(result)
@@ -35,10 +34,25 @@ result = 10  # comment
 result = str(result)
 print(type(result))
 
+# Operatii
 
+# comparison
+a = 100
+b = 100
+print(a == b)
+print('ID of a is:', id(a))
+print('ID of b is:', id(b))
+print(a is b)
 
+a = "/////////////////abcd"*4100 + "/////////////////abcd$%^*" + "/////////////////abcd$%^*"
+print(a)
+b = "/////////////////abcd"*4100 + "/////////////////abcd$%^*" + "/////////////////abcd$%^*"
+print(a == b)
+print('ID of a is:', id(a))
+print('ID of b is:', id(b))
+print(a is b)
 
-
-
-
-
+# slice
+    #0123456
+a = 'my_text'
+print(a[1])
