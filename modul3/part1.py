23d7ed4429d00d1a89cc601bf78531fa9c633640
emanuel.craciun <emanuel.craciun@savnet.ro>
2022-10-20 19:10:17 +0300

    modul 3

---
+++ modul3/part1.py
@@ -0,0 +1,52 @@
+a = 100
+b = 100
+
+if a > b:
+    print('a is larger')
+elif a > 50:
+    print('a is grater then 50')
+elif a > 60:
+    print('a is grater then 60')
+else:
+    print('b is larger')
+
+
+a = "101"
+
+if a > '100':
+    print('success >')
+elif a == '100':
+    print('success ==')
+
+
+# chr() and ord()
+
+print(ord('0'))
+print(ord('1'))
+print(ord('a'))
+print(ord('A'))
+print(chr(65))
+
+# Trueish
+
+a = "101"
+print(a > '100')
+
+if True:
+    print('this will always be true')
+
+if a:
+    print('this will also always be true')
+
+a = ""
+if a:
+    print('this will also always be false')
+else:
+    print('a is False')
+
+a = (0 + 0j)
+if a:
+    print('a is True')
+else:
+    print('a is False')
+
