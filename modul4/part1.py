4a87bf6a8769d67997fbd7ff30f2d145addf1d31
emanuel.craciun <emanuel.craciun@savnet.ro>
2022-10-27 21:48:28 +0300

    modul 4 lab 1

--- modul4/part1.py
+++ modul4/part1.py
@@ -16,14 +16,21 @@
 # print('Result is :', result)
 
 # 3! = 1*2*3
-def factorial(number, limit):
-    print(number)
-    result = 1
-    for i in range(1, number + 1):
-        if result > limit:
-            return result  # this stops function execution
-        result *= i
-    return result
+# def factorial(number, limit):
+#     print(number)
+#     result = 1
+#     for i in range(1, number + 1):
+#         if result > limit:
+#             return result  # this stops function execution
+#         result *= i
+#     return result
+#
+# result = factorial(100, 1000)
+# print('Result is :', result)
+#
 
-result = factorial(100, 1000)
-print('Result is :', result)
+# variable number of arguments
+def factorial(*args): # variable number of arguments
+    print(args)
+
+factorial(1234,1234)
