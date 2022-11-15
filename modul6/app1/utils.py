3b98aa7fd81278ded27f30abc9c7420ffc978297
emanuel.craciun <emanuel.craciun@savnet.ro>
2022-11-15 21:09:07 +0200

    modul 6

--- modul6/app1/utils.py
+++ modul6/app1/utils.py
@@ -15,14 +15,14 @@
 #         stoc_existent.update({caract_produs[0]: [float(caract_produs[1]), int(caract_produs[2])]})
 
 def adaugare_produs(stoc_existent: dict):
-    prod = input("introduceti produs")
-    caract_produs = prod.split(";")
-    if stoc_existent.get(caract_produs[0], False):
-        stoc_nou = stoc_existent[caract_produs[0]][1] + int(caract_produs[2])
-        pret_nou = float(caract_produs[1])
-        stoc_existent.update({caract_produs[0]: [pret_nou, stoc_nou]})
+    name, price, stock = input("introduceti produs").split(";")
+    stock = int(stock)
+    price = float(price)
+    if stoc_existent.get(name, False):
+        stoc_nou = stoc_existent[name][1] + stock
+        stoc_existent.update({name: [price, stoc_nou]})
     else:
-        stoc_existent.update({caract_produs[0]: [float(caract_produs[1]), int(caract_produs[2])]})
+        stoc_existent.update({name: [price, stock]})
 
 
 
