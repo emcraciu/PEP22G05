628104a87eb97a73a0139c08a2500208b7084862
emanuel.craciun <emanuel.craciun@savnet.ro>
2022-11-24 21:26:47 +0200

    modul 7

--- modul7/app1/meniu2.py
+++ modul7/app1/meniu2.py
@@ -1,3 +1,33 @@
 from app import Haine
+from app import Accesorii
 
-bluze = Haine("bluze", 10, 5)
+produse = []
+h1 = Haine("Pantaloni", 100, 3)
+a1 = Accesorii("Bratara", 100, "argint", 4)
+produse.append(h1)
+produse.append(a1)
+
+
+def vizualizare(produse):
+    for produs in produse:
+        print(produs)
+
+def add_product(produse):
+    a = input("what is the category")
+
+def meniu():
+    meniu = {"1": add_product, "2": vizualizare, "3": quit}
+    while True:
+        optiune = input("Optiuni:\n1.Adaugare produs\n2.Vizualizati\n3.Iesire\n").strip()
+        try:
+            # action = meniu[optiune]
+            if optiune == "2":
+                meniu[optiune](produse)
+            elif optiune == '1':
+                meniu[optiune](produse)
+        except KeyError:
+            print("Incorrect value! try again... ")
+        # action()
+
+
+meniu()
