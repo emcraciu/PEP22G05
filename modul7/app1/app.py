5cdf853add9f7eaab386b3c83abf8446db34e2c4
emanuel.craciun <emanuel.craciun@savnet.ro>
2022-11-29 21:04:32 +0200

    modul 7

--- modul7/app1/app.py
+++ modul7/app1/app.py
@@ -39,10 +39,36 @@ class Accesorii:
         return f
 
 
+class Incaltaminte(Haine):
+
+    def __init__(self, nume, pret, stoc, **kwargs):
+        super(Incaltaminte, self).__init__(nume, pret, stoc)
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def __str__(self):
+        f = super().__str__()
+        for attribute_ in dir(self):
+            if "__" in attribute_ or attribute_ in ["name", "pret", "stoc"]:
+                continue
+            f += f"\n {attribute_} = {getattr(self, attribute_)}"
+        return f
+
+
+class Genti(Incaltaminte):
+    pass
+
+
 if __name__ == '__main__':
-    bratara = Accesorii('Test', 'aur', 100, 50)
-    bluze = Haine("bluze", 10, 5)
-    print(bluze.pret)
-    print(bratara.material)
-    print(bluze)
-    print(bratara)
+    pantofi = Incaltaminte("pantof1", 50, 100, marime=40, material='piele', culoare="black")
+    # bratara = Accesorii('Test', 'aur', 100, 50)
+    # bluze = Haine("bluze", 10, 5)
+    # print(bluze.pret)
+    # print(bratara.material)
+    # print(bluze)
+    # print(bratara)
+
+    print(pantofi)
+
+    genti = Genti("poseta", 50, 100,  material='piele', culoare="black", acesori="nasturi")
+    print(genti)
