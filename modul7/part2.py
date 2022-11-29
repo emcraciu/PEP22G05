eac42b18b58f97fc777e63e718125b4cbc102c48
emanuel.craciun <emanuel.craciun@savnet.ro>
2022-11-29 19:30:21 +0200

    modul 7

--- modul7/part2.py
+++ modul7/part2.py
@@ -27,3 +27,26 @@ print(a.__setattr__("new_attr", "100"))
 print(a.__getattribute__("new_attr"))
 
 # Create object and set attributes from input
+
+
+class A:
+    attr_A = 'A'
+    custom = "my_custom_text"
+
+class B(A):
+    attr_B = 'B'
+    custom = 1
+
+class C(B):
+    attr_C = 'C'
+    custom = 10
+
+a = A()
+b = B()
+print(b.attr_A)
+print(b.attr_B)
+c = C()
+print(c.attr_A)
+print(c.attr_B)
+print(c.attr_C)
+print(c.custom)
